748947ba19212ed855631cd7f9f0b8eca83a2651
santilobos <santilobos21@gmail.com>
2024-02-12 17:44:36 +0100

    he terminado la tarea

---
+++ index.js
@@ -0,0 +1,81 @@
+let variableSinValor;
+let booleano1 = (true);
+let booleano2 = (false);
+
+const PI = 3.14;
+
+const TAU = PI * 2;
+
+let booleanoAnd = (booleano1===booleano2);
+
+let booleanoNot = booleano2;
+
+let booleanoMix0 = (booleano1 > booleano2);
+
+let incrementarDesp = 2;
+let resultadoDesp = (incrementarDesp ++ *1);
+
+let incrementarAntes = 2;
+let resultadoAntes = (++ incrementarAntes *1)
+
+let contarHasta10_2 = 0;
+for (contarHasta10_2 = 0; contarHasta10_2 <10; contarHasta10_2++) {
+}
+
+let postI = 0;
+let postJ = 0;
+for (let i = 0; i <11; i++) {
+    postI += postJ++; 
+}
+
+
+let sumaPares = 0
+for (let i = 0; i <10; i++) {
+
+    if (i % 2 ===0) {
+        sumaPares += i;
+    }
+} 
+
+
+let variableValorNumerico = 2
+
+const MiNombre = "Santi"
+
+const MiNumeroFav = 9
+
+let booleanoOr = (booleano1 || booleano2);
+
+let booleanoMix1 = (booleano1 && (TAU/2 === PI) || variableValorNumerico >= MiNumeroFav) 
+
+let seisNoEsNueve = (6 !== 9)
+
+let booleanoMix2 = (variableValorNumerico > 0 || variableValorNumerico - (MiNumeroFav*TAU))
+
+let valorSuma = MiNumeroFav + variableValorNumerico
+
+let valorResta = MiNumeroFav - variableValorNumerico
+
+let valorMultiplicacion = MiNumeroFav * variableValorNumerico
+
+let valorDivision = MiNumeroFav / 3
+
+contarHasta10 = 0
+while (contarHasta10 < 10) {
+    contarHasta10++;
+}
+
+let preI = 0
+let preJ = 0 
+for (let i = 0; i < 11; i++) {
+    preI += ++preJ;
+    
+}
+
+let sumaImpares = 0
+for (let i = 0; i < 10; i++) {
+    if (i % 2 !== 0) {
+        sumaImpares += i;
+    }
+    
+}
